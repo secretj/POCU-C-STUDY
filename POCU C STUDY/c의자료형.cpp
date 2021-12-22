9038eea2a1f46276eee259d2c3dfb9b2797c0fcc
secretj <qkrwlsgud890@gmail.com>
2021-12-23 02:02:58 +0900

    c의 자료형 1223 commit

---
+++ POCU C STUDY/c의자료형.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void) {
+	int int1 = 10;
+	long long1 = 100;
+	char char1 = 'A';
+	float float1 = -1.5f;
+	
+
+	printf("%d\n",int1);
+	printf("%ld\n", long1);
+	printf("%c\n", char1);
+	printf("%f\n", float1);
+
+	
+	return 0;
+}
