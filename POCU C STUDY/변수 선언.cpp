0793165fcface09cd0dcccf66dccb9bf25d76372
secretj <qkrwlsgud890@gmail.com>
2021-12-24 16:51:57 +0900

    변수 선언 commit 1224

---
+++ POCU C STUDY/변수 선언.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int num1 = 10;
+	int num2 = 1234;
+	int result;
+
+	printf("num1=%d, num2 = %d\n", num1, num2);
+
+	result = num1+ num2; /* ÄÄÆÄÀÏ*/
+
+	printf("%d+ %d = %d", num1, num2, result);
+
+	return 0;
+}
