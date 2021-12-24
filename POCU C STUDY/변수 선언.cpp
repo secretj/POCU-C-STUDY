18cf71bffd7b7d4d2d5d1208ca70e7ed42b13df2
secretj <qkrwlsgud890@gmail.com>
2021-12-24 17:59:45 +0900

    연산자 마무리 commit

--- POCU C STUDY/변수 선언.cpp
+++ POCU C STUDY/변수 선언.cpp
@@ -5,12 +5,14 @@ int main(void)
 	int num1 = 10;
 	int num2 = 1234;
 	int result;
+	int result2;
 
 	printf("num1=%d, num2 = %d\n", num1, num2);
 
 	result = num1+ num2; /* 컴파일*/
+	result2 = num1 < num2 ? num1 : num2; /*삼항연산자*/
 
-	printf("%d+ %d = %d", num1, num2, result);
+	printf("%d+ %d = %d\n%d", num1, num2, result, result2);
 
 	return 0;
 }
