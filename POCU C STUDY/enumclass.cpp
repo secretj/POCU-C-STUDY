4642c2652d16ea69a9f6dafb8ddf7bfeb10d0a64
secretj <qkrwlsgud890@gmail.com>
2021-12-24 16:17:41 +0900

    enum 1224 commit

---
+++ POCU C STUDY/enumclass.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+enum champ {
+	CHAMP_ZEN,
+	CHAMP_JAX,
+	CHAMP_VAYNE,
+	CHAMP_LULU,
+	CHAMP_LEESIN
+
+};
+
+enum role {
+	ROLE_TOP,
+	ROLE_MID,
+	ROLE_JUNGLE,
+	ROLE_BOTTOM,
+	ROLE_SUPPORTER
+};
+
+int main(void)
+{
+	enum champ my_champ = CHAMP_VAYNE;
+	enum role my_role = ROLE_BOTTOM;
+
+	printf("my_champ : %d\n", my_champ);
+	printf("my_role : %d\n", my_role);
+	printf("\n");
+
+	
+	return 0;
+}
