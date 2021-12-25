4b1a0371d279274eb47dbd8553f10255195347f6
secretj <qkrwlsgud890@gmail.com>
2021-12-25 19:53:12 +0900

    1225 float

--- POCU C STUDY/변수 선언.cpp
+++ POCU C STUDY/변수 선언.cpp
@@ -4,15 +4,16 @@ int main(void)
 {
 	int num1 = 10;
 	int num2 = 1234;
-	int result;
+	float num3 = 1.5f;
+	float result;
 	int result2;
 
-	printf("num1=%d, num2 = %d\n", num1, num2);
+	printf("num1=%d, num2 = %d, num3= %f\n", num1, num2,num3);
 
-	result = num1+ num2; /* 컴파일*/
-	result2 = num1 < num2 ? num1 : num2; /*삼항연산자*/
+	result = num1+num3; /* 컴파일*/
+	result2 = num1 < num3 ? num3 : num2; /*삼항연산자*/
 
-	printf("%d+ %d = %d\n%d", num1, num2, result, result2);
+	printf("%d+ %f = %d\n%d", num1, num3, result, result2);
 
 	return 0;
 }
