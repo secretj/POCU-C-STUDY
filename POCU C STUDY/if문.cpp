486c6ab0bf40a585605192c64cc0648d04d2fa1a
secretj <qkrwlsgud890@gmail.com>
2021-12-26 19:39:47 +0900

    if문 commit

---
+++ POCU C STUDY/if문.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void) {
+	int num1 = 10;
+	int num2 = 20;
+	if (num1 > num2) {
+		printf("num1은 num2보다 큽니다.");
+	}
+	else {
+		printf("num1은 num2보다 작습니다.");
+	}
+	return 0;
+}
